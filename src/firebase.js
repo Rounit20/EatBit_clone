844b952366ccc3fe318b96f73b84814f8f8073ef
Rounit20 <162728108+Rounit20@users.noreply.github.com>
2025-03-13 08:27:28 +0530

    Update firebase.js
    removed sensitive information

--- src/firebase.js
+++ src/firebase.js
@@ -4,15 +4,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; 
 
 // Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAbjFMBYmtkrUOpp7tNSpr7mOANiggnT-0",
-    authDomain: "eatbit-60179.firebaseapp.com",
-    projectId: "eatbit-60179",
-    storageBucket: "eatbit-60179.appspot.com",
-    messagingSenderId: "231662062816",
-    appId: "1:231662062816:web:b603a65093a315bdae5485",
-    measurementId: "G-27QD1PYJJ2"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
